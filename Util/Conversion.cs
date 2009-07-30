d996971861fa108b1a44ccb649af90c15a52abd7
alex.degroot <alex.degroot@f5845bbe-ab47-0410-b903-654cdb7b429f>
2009-07-30 19:43:41 +0000

    Missing utilities.

---
+++ Util/Conversion.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Util;
+using JetBrains.ReSharper.Psi;
+using JetBrains.Annotations;
+
+namespace AgentJohnson.Util
+{
+    public static class Conversion
+    {
+        public static TextRange TreeTextRangeToTextRange([NotNull] TreeTextRange ttr)
+        {
+            return new TextRange(ttr.StartOffset.Offset, ttr.EndOffset.Offset);
+        }
+    }
+}
---
+++ Util/Shell.cs
@@ -0,0 +1,41 @@
+﻿namespace AgentJohnson.Util
+{
+    using System;
+    using EnvDTE;
+    using JetBrains.Annotations;
+    using JetBrains.VsIntegration.Application;
+    
+    public static class Shell
+    {
+        [NotNull]
+        public static _DTE GetDte()
+        {
+            return VSShell.Instance.ServiceProvider.Dte();
+        }
+
+        public static void CreateCommand([NotNull] string name, bool execute, [CanBeNull] Action<Command> action)
+        {
+            _DTE dte = GetDte();
+
+            Command command;
+            try
+            {
+                command = dte.Commands.Item(name, -1);
+            }
+            catch
+            {
+                command = null;
+            }
+
+            if (execute && command != null)
+            {
+                dte.ExecuteCommand(name, string.Empty);
+            }
+
+            if (action != null)
+            {
+                action(command);
+            }
+        }
+    }
+}
